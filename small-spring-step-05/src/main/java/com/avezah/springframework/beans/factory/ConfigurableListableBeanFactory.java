89db826c9053a5c39b51b7cc023f0420bb0da1a6
avezah <chenqiangluck@gmail.com>
2022-11-28 17:04:54 +0800

    fix: ConfigurableBeanFactory implement wrong interface

--- small-spring-step-05/src/main/java/com/avezah/springframework/beans/factory/ConfigurableListableBeanFactory.java
+++ small-spring-step-05/src/main/java/com/avezah/springframework/beans/factory/ConfigurableListableBeanFactory.java
@@ -3,8 +3,9 @@ package com.avezah.springframework.beans.factory;
 import com.avezah.springframework.beans.BeansException;
 import com.avezah.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import com.avezah.springframework.beans.factory.config.BeanDefinition;
+import com.avezah.springframework.beans.factory.config.ConfigurableBeanFactory;
 
-public interface ConfigurableListableBeanFactory extends AutowireCapableBeanFactory, HierarchicalBeanFactory, ListableBeanFactory {
+public interface ConfigurableListableBeanFactory extends AutowireCapableBeanFactory, ConfigurableBeanFactory, ListableBeanFactory {
 
     BeanDefinition getBeanDefinition(String beanName) throws BeansException;
 }
